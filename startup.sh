9be41b63af97fdca642c75079c278c05c01ee3cf
gburgosw <gburgosw@gmail.com>
2017-12-18 11:55:39 -0300

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export GOOGLE_APPLICATION_CREDENTIALS=/home/rita/Escritorio/GRita/a.json
+gnome-terminal --full-screen --hide-menubar --profile=prRita -e "python /home/rita/Escritorio/GRita/ritaG.py"
